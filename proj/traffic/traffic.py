9d6904dfdba75a5d42786a35b17daea7ba30b3ea
SSSayon <1697295548@qq.com>
2024-02-06 21:37:43 +0800

    proj Traffic finished

--- proj/traffic/traffic.py
+++ proj/traffic/traffic.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2 as cv
 import numpy as np
 import os
 import sys
@@ -58,7 +58,22 @@ def load_data(data_dir):
     be a list of integer labels, representing the categories for each of the
     corresponding `images`.
     """
-    raise NotImplementedError
+    images = []
+    labels = []
+    dirs = os.listdir(data_dir)
+    for category in dirs:
+        dir = os.path.join(data_dir, category)
+        files = os.listdir(dir)
+
+        for file in files:
+            img = cv.imread(os.path.join(dir, file))
+            img = cv.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+            img = np.array(img)
+
+            images.append(img)
+            labels.append(category)
+
+    return (images, labels)
 
 
 def get_model():
@@ -67,7 +82,29 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+    model = tf.keras.models.Sequential([
+
+        tf.keras.layers.Conv2D(
+            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
+
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+        tf.keras.layers.Flatten(),
+
+        tf.keras.layers.Dense(128, activation="relu"),
+
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
+
+    ])
+
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    return model
 
 
 if __name__ == "__main__":
